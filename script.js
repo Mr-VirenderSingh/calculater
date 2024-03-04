7ec4180551df7454c4ee089ba9548382b32b5f93
Vireder <virendersd621@gmail.com>
2024-03-04 20:02:23 +0530

    add

--- script.js
+++ script.js
@@ -1,47 +1,10 @@
-//const button = document.querySelectorAll("button");
-// const input = document.querySelector("input");
-// let display_Digit = "";
-
-// button.forEach((event) =>
-//   event.addEventListener("click", (e) => {
-//     let equation = e.target.innerText;
-//     if (equation == "AC") {
-//       input.value = "";
-//       display_Digit = "";
-//     } else if (equation === "DEL") {
-//       display_Digit = display_Digit.slice(0, -1);
-//       input.value = display_Digit;
-//       console.log(display_Digit);
-//     } else if (equation === "=") {
-//       display_Digit = eval(display_Digit);
-//       input.value = display_Digit;
-//     } else {
-//       calculate(equation);
-//     }
-//   })
-// );
-
-// const calculate = (equation) => {
-//   display_Digit += equation;
-
-//   input.value = display_Digit;
-// };
-
 const button = document.querySelectorAll("button");
 const input = document.querySelector("input");
 let display_Digit = "";
-let isCalculated = false; // Flag to check if a calculation has been performed
 
 button.forEach((event) =>
   event.addEventListener("click", (e) => {
     let equation = e.target.innerText;
-    if (isCalculated && equation !== "=" && equation !== "AC" && equation !== "DEL") {
-      // If a calculation has been performed and the next button clicked is not "=", "AC", or "DEL"
-      // then clear the input
-      input.value = "";
-      display_Digit = "";
-      isCalculated = false; // Reset the flag
-    }
     if (equation == "AC") {
       input.value = "";
       display_Digit = "";
@@ -52,7 +15,6 @@ button.forEach((event) =>
     } else if (equation === "=") {
       display_Digit = eval(display_Digit);
       input.value = display_Digit;
-      isCalculated = true; // Set the flag to true after a calculation
     } else {
       calculate(equation);
     }
@@ -61,6 +23,44 @@ button.forEach((event) =>
 
 const calculate = (equation) => {
   display_Digit += equation;
+
   input.value = display_Digit;
 };
+
+// const button = document.querySelectorAll("button");
+// const input = document.querySelector("input");
+// let display_Digit = "";
+// let isCalculated = false; // Flag to check if a calculation has been performed
+
+// button.forEach((event) =>
+//   event.addEventListener("click", (e) => {
+//     let equation = e.target.innerText;
+//     if (isCalculated && equation !== "=" && equation !== "AC" && equation !== "DEL") {
+//       // If a calculation has been performed and the next button clicked is not "=", "AC", or "DEL"
+//       // then clear the input
+//       input.value = "";
+//       display_Digit = "";
+//       isCalculated = false; // Reset the flag
+//     }
+//     if (equation == "AC") {
+//       input.value = "";
+//       display_Digit = "";
+//     } else if (equation === "DEL") {
+//       display_Digit = display_Digit.slice(0, -1);
+//       input.value = display_Digit;
+//       console.log(display_Digit);
+//     } else if (equation === "=") {
+//       display_Digit = eval(display_Digit);
+//       input.value = display_Digit;
+//       isCalculated = true; // Set the flag to true after a calculation
+//     } else {
+//       calculate(equation);
+//     }
+//   })
+// );
+
+// const calculate = (equation) => {
+//   display_Digit += equation;
+//   input.value = display_Digit;
+// };
 
